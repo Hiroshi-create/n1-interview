16e9676bdb8e0452a4a039f48cae20c530136b04
Hiroshi-create <hi130404@gmail.com>
2025-01-16 19:09:44 +0900

    individualReportコレクションが作成されると、collectInterviewsCountフィールドが+1されるように修正

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -5,32 +5,23 @@ const logger = require("firebase-functions/logger");
 admin.initializeApp();
 
 exports.updateCollectInterviewsCount = functions.firestore
-  .document('themes/{themeId}/interviews/{interviewId}')
-  .onWrite(async (change, context) => {
+  .document('themes/{themeId}/interviews/{interviewId}/individualReport/{reportId}')
+  .onCreate(async (snapshot, context) => {
     const themeId = context.params.themeId;
-    // const newValue = change.after.data();
-    // const oldValue = change.before.data();
-
-    // individualReportサブコレクションが作成されたか、reportCreatedがtrueになった場合
-    if (
-    //   (newValue && newValue.reportCreated && !oldValue?.reportCreated) ||
-      (change.after.ref.collection('individualReport').id !== change.before.ref.collection('individualReport').id)
-    ) {
-      const themeRef = admin.firestore().collection('themes').doc(themeId);
-      
-      try {
-        await admin.firestore().runTransaction(async (transaction) => {
-          const themeDoc = await transaction.get(themeRef);
-          const currentCount = themeDoc.data().collectInterviewsCount || 0;
-          
-          transaction.update(themeRef, {
-            collectInterviewsCount: currentCount + 1
-          });
+    const themeRef = admin.firestore().collection('themes').doc(themeId);
+    
+    try {
+      await admin.firestore().runTransaction(async (transaction) => {
+        const themeDoc = await transaction.get(themeRef);
+        const currentCount = themeDoc.data().collectInterviewsCount || 0;
+        
+        transaction.update(themeRef, {
+          collectInterviewsCount: currentCount + 1
         });
-        logger.info(`Successfully updated collectInterviewsCount for theme ${themeId}`);
-      } catch (error) {
-        logger.error(`Error updating collectInterviewsCount for theme ${themeId}:`, error);
-      }
+      });
+      logger.info(`Successfully updated collectInterviewsCount for theme ${themeId}`);
+    } catch (error) {
+      logger.error(`Error updating collectInterviewsCount for theme ${themeId}:`, error);
     }
     
     return null;
